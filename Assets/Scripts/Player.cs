776141636d78127626bb7b81106169223918dda7
zeFerret <justinas.seskevicius@gmail.com>
2019-03-17 15:48:22 +0200

    feat: crossplatform input & horizontal movement & tile/player colliders

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 10f;
+
+    Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        FlipSprite();
+    }
+
+    private void Move()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(horizontalThrow * movementSpeed, rigidbody2D.velocity.y);
+        rigidbody2D.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidbody2D.velocity.x), transform.localScale.y);
+        }
+    }
+}
