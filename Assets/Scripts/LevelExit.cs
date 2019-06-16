171d2ad55006ca0331f297cc0cb86d67cd24be20
zeFerret <justinas.seskevicius@gmail.com>
2019-06-16 16:20:58 +0300

    feat: add level exit & transitioning levels

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] ParticleSystem exitParticleSystem;
+    [SerializeField] float levelExitTime = 2f;
+
+    bool inTransition = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!inTransition)
+        {
+            inTransition = true;
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        exitParticleSystem.Play();
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        yield return new WaitForSeconds(levelExitTime);
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
